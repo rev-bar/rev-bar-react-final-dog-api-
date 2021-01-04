d31289ace8db8a44df846702d60700c6e0096889
rev bar <revital.jer@gmail.com>
2021-01-04 10:31:17 +0200

    istalling bootstrap, axios, router+ adding home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/Homepage/HomePage';
+
+
+// import ActorsPage from './pages/ActorPage';
+// import HomePage from './pages/HomePage';
+// import MoviePage from './pages/MoviePage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <HashRouter>
+          <Switch>
+            <Route exact path= "/"><HomePage/></Route>
+          </Switch>  
+        </HashRouter> 
+ 
     </div>
   );
 }
---
+++ src/pages/Homepage/HomePage.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+
+function HomePage(props) {
+    
+    return (
+        <div className= "p-home">
+            <p className= "p-home-head">DogBook</p>  
+            <h1>Mens best friend</h1>
+                <div>
+                    {/* <Link to= "/breeds"  className= "p-home-links">Breeds page</Link>   */}
+                    <p>ff</p>
+                </div>
+            
+                 
+        </div>
+    );
+}
+
+export default HomePage;
