9448a701b057ea5de8d4682abeee35b513a9e869
rev bar <revital.jer@gmail.com>
2021-01-05 14:12:58 +0200

    adding filter to BreedsPage

--- src/pages/BreedsPage/BreedsPage.js
+++ src/pages/BreedsPage/BreedsPage.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Button, Row } from 'react-bootstrap';
+import { Button, Form, Row } from 'react-bootstrap';
 import DogCard from '../../components/DogCard/DogCard';
 import DogNavBar from '../../components/NavBar/NavBar';
+import SearchBox from '../../components/SearchBox/SearchBox';
 
 import './BreedsPage.css';
 
@@ -10,7 +11,9 @@ function BreedsPage() {
 
     // const {breeds} =props;
     const [breeds,setBreeds] = useState([]);
+    const [breedFilter,setBreedFilter] = useState("");
     const [reRender,setReRender] = useState(5);
+
 
 
         useEffect ( ()=> {
@@ -24,22 +27,26 @@ function BreedsPage() {
         // alert("renderr");  
     }
 
-
+    
     //prepering data for render:
+    const filteredBreeds = breeds.filter(breed => breed.toLowerCase().includes( breedFilter.toLowerCase()) )
 
-    const dogCards = breeds.map((breed,index) => <DogCard breed= {breed} index= {index} key= {index} reRender= {reRender} /> )
+    const dogCards = filteredBreeds.map((breed,index) => <DogCard breed= {breed} index= {index} key= {index} reRender= {reRender} /> )
 
     return (
-        <div>
+        <>
             <DogNavBar></DogNavBar>
             <Row m={6} className="flex">
+                <Form.Group> 
+                    <Form.Control  value = {breedFilter} onChange= {e=> setBreedFilter(e.target.value) } as="textarea" className= "p-actors-search" placeholder= "Search actor" />
+                </Form.Group>
                 <Button onClick={reRenderPics} >Update</Button>
             </Row>
             <Row m={6} className="flex">
-            {dogCards}
+                {dogCards}
             </Row>
             
-        </div>
+        </>
     );
 }
 
